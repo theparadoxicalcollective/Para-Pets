df1cb4f1ad75fbb1e3546cfde9fb2dd400d00139
theparadoxicalcollective <the.paradoxical.collective@gmail.com>
2026-08-26 22:40:46 -0500

    Fix costume route registration formatting

--- server/routes.ts
+++ server/routes.ts
@@ -48,7 +48,8 @@ import { registerClearingEquipmentRoutes } from "./routes/clearingEquipment.rout
 import { registerClearingAdminRoutes } from "./routes/clearingAdmin.routes";
 import { registerClearingShopRoutes } from "./routes/clearingShop.routes";
 import { registerSoulExchangeRoutes } from "./routes/soulExchange.routes";
-import { registerCostumeAdminRoutes } from "./routes/costumeAdmin.routes";\nimport { registerCostumePlayerRoutes } from "./routes/costumePlayer.routes";
+import { registerCostumeAdminRoutes } from "./routes/costumeAdmin.routes";
+import { registerCostumePlayerRoutes } from "./routes/costumePlayer.routes";
 import { getEffectivePetLayer } from "@shared/petLayer";
 
 type ShopPurchaseTransaction = Parameters<Parameters<typeof db.transaction>[0]>[0];
@@ -821,7 +822,8 @@ export async function registerRoutes(
   registerSoulExchangeRoutes(app, { isAuthenticated });
   registerClearingEquipmentRoutes(app, { db, storage, isAuthenticated });
   registerClearingAdminRoutes(app, { db, isAdmin });
-  registerCostumeAdminRoutes(app);\n  registerCostumePlayerRoutes(app);
+  registerCostumeAdminRoutes(app);
+  registerCostumePlayerRoutes(app);
 
   const marketplaceRouteDependencies: MarketplaceRouteDependencies = {
     storage,
